e0c472b7db34e7cdf640f7ff61c8c691a55f249a
kuujoo <kuujoo@gmail.com>
2020-10-18 20:03:25 +0300

    Add files.

---
+++ OgmoLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace kuujoo.Pixel
+{
+    public class OgmoLevel : OgmoValueContainer
+    {
+        public string OgmoVersion { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int OffsetX { get; set; }
+        public int OffsetY { get; set; }
+        public OgmoLayer[] Layers { get; set; }
+        public List<string> GetTilesets()
+        {
+            var tilesets = new List<string>();
+            foreach (var l in Layers)
+            {
+                if (l.Type == OgmoLayerType.Tile)
+                {
+                    if (!tilesets.Contains(l.Tileset))
+                    {
+                        tilesets.Add(l.Tileset);
+                    }
+                }
+            }
+            return tilesets;
+        }
+    }
+}
---
+++ OgmoLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace kuujoo.Pixel
+{
+    public static class OgmoLoader
+    {
+        public static OgmoLevel LoadLevel(string path)
+        {
+            var data = File.ReadAllText(path);
+            if (data.Length == 0) return null;
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return JsonSerializer.Deserialize<OgmoLevel>(data, options);
+        }
+    }
+}
