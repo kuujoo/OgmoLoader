7e68150776ba0a28f28623bcfe9de78287a23433
kuujoo <kuujoo@gmail.com>
2020-10-19 23:39:46 +0300

    Few functions to OgmoLoader class.
    -save / load world
    -load level from pre-read data

--- OgmoLoader.cs
+++ OgmoLoader.cs
@@ -10,11 +10,26 @@ namespace kuujoo.Pixel
         {
             var data = File.ReadAllText(path);
             if (data.Length == 0) return null;
+            return LoadFromData(data);
+        }
+        public static OgmoLevel LoadFromData(string data)
+        {
             var options = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
             return JsonSerializer.Deserialize<OgmoLevel>(data, options);
+        }
+        public static OgmoWorld LoadWorld(string path)
+        {
+            var data = File.ReadAllText(path);
+            if (data.Length == 0) return null;
+            return JsonSerializer.Deserialize<OgmoWorld>(data);
+        }
+        public static void SaveWorld(string file, OgmoWorld world)
+        {
+            var data = JsonSerializer.Serialize<OgmoWorld>(world);
+            File.WriteAllText(file, data);
         }
     }
 }
