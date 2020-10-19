0d0bb1570465ec8a78016141243eca8d8b0af36b
kuujoo <kuujoo@gmail.com>
2020-10-19 23:37:34 +0300

    Add OgmoWorld class.
    Basically list of levels. Might be useful later.

---
+++ OgmoWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kuujoo.Pixel
+{
+    public class OgmoWorld
+    {
+        public List<OgmoLevel> Levels { get; set; }
+        public OgmoWorld()
+        {
+            Levels = new List<OgmoLevel>();
+        }
+    }
+}
