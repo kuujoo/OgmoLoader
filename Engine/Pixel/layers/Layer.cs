12b79f655deb9469ea332b58c12af957da748a8b
Joonas Kuusela <kuujoo@gmail.com>
2021-01-02 19:25:35 +0200

    Add concept of layer

---
+++ Engine/Pixel/layers/Layer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace kuujoo.Pixel
+{
+    public abstract class Layer : IComparable<Layer>
+    {
+        public int Id { get; set; }
+        public abstract void Render(Camera camera);
+        public abstract void Update();
+        public abstract void CleanUp();
+        public abstract void OnGraphicsDeviceReset();
+
+        public int CompareTo(Layer other)
+        {
+            return Id.CompareTo(other.Id);
+        }
+    }
+}
