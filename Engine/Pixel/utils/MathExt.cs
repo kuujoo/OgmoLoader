0cd77c4001902aea70e7d82af05b582066739cc8
Joonas Kuusela <kuujoo@gmail.com>
2021-01-02 22:56:32 +0200

    Approach method.

---
+++ Engine/Pixel/utils/MathExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kuujoo.Pixel
+{
+    public static class MathExt
+    {
+		public static float Approach(float start, float end, float shift)
+		{
+			if (start < end)
+			{
+				return Math.Min(start + shift, end);
+			}
+			return Math.Max(start - shift, end);
+		}
+	}
+}
