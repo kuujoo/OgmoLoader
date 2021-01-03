ac06a37560968018a1afd27f02a4e57a7384e5e4
Joonas Kuusela <kuujoo@gmail.com>
2021-01-04 00:40:41 +0200

    Fix PixelSpriteResource sample.

--- Engine/PixelSpriteResourceSample/Game1.cs
+++ Engine/PixelSpriteResourceSample/Game1.cs
@@ -19,25 +19,6 @@ namespace kuujoo.Pixel
             graphics.DrawSprite(Position, _sprite, Color.White);
         }
     }
-    public class SpriteComponent : Component
-    {
-        Sprite _sprite;
-
-        public SpriteComponent()
-        {
-        }
-        public override void Initialize()
-        {
-            base.Initialize();
-            _sprite = Entity.Scene.GetSceneComponent<SpriteResources>().GetSprite(Game1.SheetName, "Checker");
-        }
-        public override void Render(Graphics graphics)
-        {
-            base.Render(graphics);
-            graphics.DrawSprite(Entity.Position, _sprite, Color.White);
-        }
-    }
-
     public class Game1 : Engine
     {
         public static string SheetName = "Content/Sprites/sheet.ase";
@@ -59,7 +40,10 @@ namespace kuujoo.Pixel
             room.CreateEntityLayer(0, "entities");
             // Entity & Component - preferred
             var entity = room.CreateEntity(0);
-            entity.AddComponent(new SpriteComponent());
+            entity.AddComponent(new SpriteComponent() 
+            {
+                Sprite = room.GetSceneComponent<SpriteResources>().GetSprite(Game1.SheetName, "Checker")
+            });
             entity.Position = new Vector2(50, 50);
             // Subclassed entity, fast and easy
             var entity2 = room.AddEntity(0, new SpriteEntity());
