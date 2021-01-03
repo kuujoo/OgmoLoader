2a66e8d9ba1d52731f63f90cd63ae46c5a1dbb53
Joonas Kuusela <kuujoo@gmail.com>
2021-01-04 00:32:58 +0200

    SpriteComponent & SettingsComponents

---
+++ Engine/Pixel/ec/Components/SettingsComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace kuujoo.Pixel
+{
+    public abstract class SettingsComponent : Component
+    {
+        public void SetBool(string key, bool value)
+        {
+            SetValueWithRecflection(key, value);
+        }
+        public void SetInt(string key, int value)
+        {
+            SetValueWithRecflection(key, value);
+        }
+        public void SetString(string key, string value)
+        {
+            SetValueWithRecflection(key, value);
+        }
+        public void SetPoint(string key, int x, int y)
+        {
+            var v = new Vector2(x, y);
+            SetPoint(key, v);
+        }
+        public void SetPoint(string key, Vector2 position)
+        {
+            SetValueWithRecflection(key, position);
+        }
+        private void SetValueWithRecflection(string key, object value)
+        {
+            var prop = GetType().GetProperty(key);
+            if (prop != null)
+            {
+                prop.SetValue(this, value);
+            }
+        }
+    }
+}
---
+++ Engine/Pixel/ec/Components/SpriteComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kuujoo.Pixel
+{
+    public class SpriteComponent : Component
+    {
+        public Sprite Sprite { get; set; }
+        public SpriteComponent()
+        {
+
+        }
+        public override void Render(Graphics graphics)
+        {
+            base.Render(graphics);
+            if (Sprite == null) return;
+
+            graphics.DrawSprite(Entity.Position - Sprite.Pivot, Sprite, Color.White);
+        }
+    }
+}
