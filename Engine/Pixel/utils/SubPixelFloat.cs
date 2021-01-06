2840e038433229a0990bdab2e09511e300b3d71b
Joonas Kuusela <kuujoo@gmail.com>
2021-01-06 23:48:12 +0200

    SubpixelFloat struct

---
+++ Engine/Pixel/utils/SubPixelFloat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kuujoo.Pixel
+{
+    public struct SubpixelFloat
+    {
+        float _remainder;
+        public float Update(float amount)
+        {
+            _remainder += amount;
+            var t = (int)Math.Truncate(_remainder);
+            _remainder -= t;
+            return t;
+        }
+        public void Reset()
+        {
+            _remainder = 0;
+        }
+    }
+}
