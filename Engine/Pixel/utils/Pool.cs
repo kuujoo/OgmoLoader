39e920daccbe22374c0d343043c8dd24cab8ed49
Joonas Kuusela <kuujoo@gmail.com>
2021-02-11 16:00:51 +0200

    Add unstatic pool.

---
+++ Engine/Pixel/utils/Pool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace kuujoo.Pixel
+{
+    public class Pool<T> where T : new()
+	{
+		private Queue<T> _pool = new Queue<T>(10);
+		public void Clear()
+		{
+			_pool.Clear();
+		}
+		public T Obtain()
+		{
+			if (_pool.Count > 0)
+			{
+				return _pool.Dequeue();
+			}
+			return new T();
+		}		
+		public void Free(T obj)
+		{
+			_pool.Enqueue(obj);
+			if (obj is IResettable)
+			{
+				((IResettable)obj).Reset();
+			}
+		}
+	}
+}
