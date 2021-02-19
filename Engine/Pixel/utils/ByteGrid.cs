aaaf0dc0a234eb0ebd6cb72571518f96cc363521
Joonas Kuusela <kuujoo@gmail.com>
2021-02-20 00:22:23 +0200

    Update grid

--- Engine/Pixel/utils/ByteGrid.cs
+++ Engine/Pixel/utils/ByteGrid.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace kuujoo.Pixel
 {
@@ -10,6 +12,7 @@ namespace kuujoo.Pixel
         byte GetValueByIndex(int idx);
         void SetValue(int x, int y, byte value);
         void SetValueByIndex(int index, byte value);
+        void SetFrom(IGrid grid, int x, int y);
     }
 
     [Serializable]
@@ -26,6 +29,7 @@ namespace kuujoo.Pixel
         }
         public byte GetValue(int x, int y)
         {
+            if (x < 0 || y < 0 || x > Width - 1 || y > Height - 1) return 0;
             return GetValueByIndex(y * Width + x);
         }
         public byte GetValueByIndex(int idx)
@@ -39,6 +43,49 @@ namespace kuujoo.Pixel
         public void SetValueByIndex(int index, byte value)
         {
             _grid[index] = value;
+        }
+        public void SetFrom(IGrid grid, int x, int y)
+        {
+            for(var i = 0; i < grid.Width; i++)
+            {
+                for(var j = 0; j < grid.Height; j++)
+                {
+                    SetValue(i + x, j + y, grid.GetValue(i, j));
+                }
+            }
+        }
+        public void SetValueByRange(Point p0, Point size, byte value)
+        {
+            for(var i = p0.X; i < p0.X + size.X; i++)
+            {
+                for (var j = p0.Y; j < p0.Y + size.Y; j++)
+                {
+                    SetValue(i, j, value);
+                }
+            }
+        }
+        public void SetReverseFrom(IGrid grid, int x, int y)
+        {
+            for (var i = 0; i < grid.Width; i++)
+            {
+                for (var j = 0; j < grid.Height; j++)
+                {
+                    SetValue(i + x, j + y, grid.GetValue( (grid.Width-1) - i, j));
+                }
+            }
+        }
+        public void SetBorders(byte value)
+        {
+            for (var i = 0; i < Width; i++)
+            {
+                SetValue(i, 0, value);
+                SetValue(i, Height - 1, value);
+            }
+            for (var i = 0; i < Height; i++)
+            {
+                SetValue(0, i, value);
+                SetValue(Width - 1, i, value);
+            }
         }
     }
 }
